fc9e26ceea5066a0684b18bcc5a1ffb921112d64
Natasha Flores Viteri <natashaflores34@gmail.com>
2024-08-22 11:35:17 +0200

    rails new

---
+++ config/routes.rb
@@ -0,0 +1,23 @@
+Rails.application.routes.draw do
+  get 'movies/title:string'
+  get 'movies/overview:text'
+  get 'movies/poster_url:string'
+  get 'movies/rating:float'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+
+  root to: 'lists#index'
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+    resources :reviews, only: :create
+  end
+
+  resources :bookmarks, only: :destroy
+  resources :reviews, only: :destroy
+end
